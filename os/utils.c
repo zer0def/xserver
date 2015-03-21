0e0058b6a0851c0cbea849bdda3fd98257c7108e
Alan Coopersmith <alan.coopersmith@oracle.com>
2015-03-21 09:28:07 -0700

    Add no-fail equivalents of allocarray & reallocarray
    v2: Remove extra 's' from comment
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Reviewed-by: Matt Turner <mattst88@gmail.com>

--- os/utils.c
+++ os/utils.c
@@ -1062,6 +1062,16 @@ XNFrealloc(void *ptr, unsigned long amount)
     return ret;
 }
 
+void *
+XNFreallocarray(void *ptr, size_t nmemb, size_t size)
+{
+    void *ret = reallocarray(ptr, nmemb, size);
+
+    if (!ret)
+        FatalError("XNFreallocarray: Out of memory");
+    return ret;
+}
+
 void
 Xfree(void *ptr)
 {
